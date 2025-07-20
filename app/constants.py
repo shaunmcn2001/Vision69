68d2f93ddc1a8c15b8588ffcef8206740d18ce56
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-20 19:20:44 +1000

    Create constants.py

---
+++ app/constants.py
@@ -0,0 +1,10 @@
+# NSW & QLD ArcGIS endpoints, kept in one place
+NSW_PARCEL_URL = (
+    "https://maps.six.nsw.gov.au/arcgis/rest/services/public/"
+    "NSW_Cadastre/MapServer/9/query"
+)
+
+QLD_PARCEL_URL = (
+    "https://spatial-gis.information.qld.gov.au/arcgis/rest/services/"
+    "PlanningCadastre/LandParcelPropertyFramework/MapServer/4/query"
+)

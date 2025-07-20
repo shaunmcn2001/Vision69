8cf9bbff1e00015c9ef387a28635195ecd48f6f1
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-20 23:31:37 +1000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import 'leaflet/dist/leaflet.css';
 import App from './App.jsx';
 import './App.css';          // global stylesheet
 

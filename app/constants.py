79f8d8d302071aa90750e17b7d2a8ca99acc6c93
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-20 22:11:31 +1000

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,9 +1,14 @@
-# NSW & QLD ArcGIS endpoints, kept in one place
+"""
+Centralised constants for external data sources.
+"""
+
+# NSW Lot layer (ID 9) – returns GeoJSON when f=geoJSON
 NSW_PARCEL_URL = (
     "https://maps.six.nsw.gov.au/arcgis/rest/services/public/"
     "NSW_Cadastre/MapServer/9/query"
 )
 
+# QLD Land Parcel framework layer (ID 4)
 QLD_PARCEL_URL = (
     "https://spatial-gis.information.qld.gov.au/arcgis/rest/services/"
     "PlanningCadastre/LandParcelPropertyFramework/MapServer/4/query"

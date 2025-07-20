9ea94d9adb6f8d0a3296c804c005cc63a09b7a9b
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-20 19:08:04 +1000

    Update ParcelMap.jsx

--- frontend/src/ParcelMap.jsx
+++ frontend/src/ParcelMap.jsx
@@ -4,18 +4,19 @@ import 'leaflet/dist/leaflet.css';
 export default function ParcelMap({ children }) {
   return (
     <MapContainer
-      className="map"
-      center={[-27.467, 153.028]}   // Brisbane CBD – tweak if you like
+      className="map"                 /* .map has height:100vh in App.css */
+      center={[-27.467, 153.028]}     /* Brisbane CBD – change if you like */
       zoom={10}
       scrollWheelZoom
       style={{ height: '100vh', width: '100%' }}
     >
+      {/* Basemap (HTTPS so Render won’t block it) */}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="© OpenStreetMap contributors"
       />
 
-      {/* dynamic GeoJSON etc. from <App /> */}
+      {/* Dynamic GeoJSON / markers from <App /> */}
       {children}
     </MapContainer>
   );

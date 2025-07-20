983681f7cf2045d6538da18a710e002e666e64e4
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-20 19:07:44 +1000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'leaflet/dist/leaflet.css';     // Leaflet layout rules
-import './index.css';                 // your own styles
+
+import 'leaflet/dist/leaflet.css';   // Leaflet layout rules
+import './index.css';
+
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(

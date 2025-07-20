d646b38806897784ec87d6586fa80b252aaac4a0
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-20 22:45:23 +1000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import 'leaflet/dist/leaflet.css';   // Leaflet layout rules
-import './index.css';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './App.css';             // <-- add this
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
 );
